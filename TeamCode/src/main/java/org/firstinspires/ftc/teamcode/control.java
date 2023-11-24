3df0a1cdc0f39b5f64185bb666877da62231d377
PathadonAougsk <PathadonAougsk2249@gmail.com>
2023-11-24 19:00:39 +0700

    Add_More readable code.. So please leave me alone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control.java
@@ -50,24 +50,55 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
+// copyright to do whatever you fucking want.
 @TeleOp(name="control", group="Linear OpMode")
-// wheel speed
 public class control extends LinearOpMode {
+    double range_motor = 0;
+    DcMotorEx Motor01, Motor02, Motor03, Motor04, _GearMotor;
+    Servo servo_left, servo_right, arm_right, arm_left;
+
+    boolean isRightBumperPressed, isLeftBumperPressed;
+    double isRightTrigger,isLeftTrigger;
+    double velocity_still, Xcontroller, Ycontroller = 0;
+    double servoPos;
+    double velocity = 1400;
 
     @Override
+    //gonna need to fix to servo like real fast.
     public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
+        initializeHardware();
+        waitForStart();
 
-        // Player One control Movement + Paper plane
-        // Player Two control Servo + hand
-        ElapsedTime runtime = new ElapsedTime();
+        while (opModeIsActive()) {
+            handleGamepadInput();
+            telemetryUpdates();
+            servoPos = servo_left.getPosition();
 
-        DcMotorEx Motor01 = hardwareMap.get(DcMotorEx.class, "Motor01");
-        DcMotorEx Motor02 = hardwareMap.get(DcMotorEx.class, "Motor02");
-        DcMotorEx Motor03 = hardwareMap.get(DcMotorEx.class, "Motor03");
-        DcMotorEx Motor04 = hardwareMap.get(DcMotorEx.class, "Motor04");
-        DcMotorEx _GearMotor = hardwareMap.get(DcMotorEx.class, "Motor05");
+            if (gamepad2.left_bumper){
+                range_motor += 0.005f;
+                servo_left.setPosition(range_motor);
+                servo_right.setPosition(range_motor);
+            }
+
+            if (gamepad2.right_bumper){
+                range_motor -= 0.005f;
+                servo_right.setPosition(range_motor);
+            }
+        }
+    }
+
+    private void initializeHardware() {
+        Motor01 = hardwareMap.get(DcMotorEx.class, "Motor01");
+        Motor02 = hardwareMap.get(DcMotorEx.class, "Motor02");
+        Motor03 = hardwareMap.get(DcMotorEx.class, "Motor03");
+        Motor04 = hardwareMap.get(DcMotorEx.class, "Motor04");
+
+//        _GearMotor = hardwareMap.get(DcMotorEx.class, "Motor05");
+
+        servo_left = hardwareMap.get(Servo.class, "Servo_left");
+        servo_right = hardwareMap.get(Servo.class, "Servo_right");
+        arm_right = hardwareMap.get(Servo.class, "Servo_arm1");
+        arm_left = hardwareMap.get(Servo.class, "Servo_arm2");
 
         Motor01.setDirection(DcMotor.Direction.FORWARD);
         Motor02.setDirection(DcMotor.Direction.REVERSE);
@@ -79,86 +110,78 @@ public class control extends LinearOpMode {
         Motor03.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         Motor04.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        Servo servo_left = hardwareMap.get(Servo.class, "Servo_left");
-        Servo servo_right = hardwareMap.get(Servo.class, "Servo_right");
-        Servo arm_right = hardwareMap.get(Servo.class, "Servo_arm1");
-        Servo arm_left = hardwareMap.get(Servo.class, "Servo_arm2");
-
         servo_right.setDirection(Servo.Direction.REVERSE);
-        double velocity_still = 0;
-//      Go calculate that shit.
-        double velocity_forward = 1400;
-        double velocity_backward = -1400;
-        double brake = 0;
-        double range_motor = 0;
+    }
 
-        double x = gamepad1.left_stick_x;
-        double y = gamepad1.left_stick_y;
+    private void handleGamepadInput() {
+        Xcontroller = gamepad1.left_stick_x;
+        Ycontroller = gamepad1.left_stick_y;
 
-        double rotate = gamepad1.right_stick_x;
+        // Rest of your gamepad input handling...
+        isLeftBumperPressed = gamepad2.left_bumper;
+        isRightBumperPressed = gamepad2.right_bumper;
+        isRightTrigger = gamepad2.right_trigger;
+        isLeftTrigger = gamepad2.left_trigger;
 
-        waitForStart();
 
-        while (opModeIsActive()) {
-
-            if (gamepad1.left_bumper){
-                range_motor = range_motor + 0.005;
-                servo_left.getPosition();
-                servo_right.getPosition();
-                servo_left.setPosition(range_motor);
-                servo_right.setPosition(range_motor);
-            }
-
-            if (gamepad1.right_bumper){
-                range_motor = range_motor - 0.005;
-                servo_left.setPosition(range_motor);
-                servo_right.setPosition(range_motor);
-            }
-
-            if (gamepad1.x){
-                // Let's check for the encoder for the height so we can set the limit
-                // if (encoder < height){
-                _GearMotor.setVelocity(velocity_forward);
-            }
-
-            if (gamepad1.a){
-                // if (encoder > miniHeight)
-                _GearMotor.setVelocity(velocity_backward);
-            }
-
-            if(gamepad1.dpad_up){
-                Motor01.setVelocity(velocity_forward);
-                Motor02.setVelocity(velocity_forward);
-                Motor03.setVelocity(velocity_forward);
-                Motor04.setVelocity(velocity_forward);
-            }
-
-            if(gamepad1.dpad_down){
-                Motor01.setVelocity(velocity_backward);
-                Motor02.setVelocity(velocity_backward);
-                Motor03.setVelocity(velocity_backward);
-                Motor04.setVelocity(velocity_backward);
-            }
-
-            if(gamepad1.dpad_left){
-                Motor01.setVelocity(velocity_backward);
-                Motor02.setVelocity(velocity_backward);
-                Motor03.setVelocity(velocity_forward);
-                Motor04.setVelocity(velocity_forward);
-            }
-
-            if(gamepad1.dpad_right){
-                Motor01.setVelocity(velocity_forward);
-                Motor02.setVelocity(velocity_forward);
-                Motor03.setVelocity(velocity_backward);
-                Motor04.setVelocity(velocity_backward);
-            }
-
-            telemetry.addData("time running", "Run Time: " + runtime.toString());
-            telemetry.addData("Currently at",  " at %7d :%7d",
-                    Motor01.getCurrentPosition(), Motor02.getCurrentPosition());
-            telemetry.addData("Currently at",  " at %7f :%7f",servo_left.getPosition(), servo_right.getPosition());
-            telemetry.update();
+        // this hard coding is killing me
+        if (Ycontroller == 1){
+            Motor01.setVelocity(velocity);
+            Motor02.setVelocity(velocity);
+            Motor03.setVelocity(velocity);
+            Motor04.setVelocity(velocity);
+        } else if (Ycontroller == -1){
+            Motor01.setVelocity(-velocity);
+            Motor02.setVelocity(-velocity);
+            Motor03.setVelocity(-velocity);
+            Motor04.setVelocity(-velocity);
+        } else if (Xcontroller == 1){
+            Motor01.setVelocity(velocity);
+            Motor02.setVelocity(velocity);
+            Motor03.setVelocity(-velocity);
+            Motor04.setVelocity(-velocity);
+        } else if (Xcontroller == -1){
+            Motor01.setVelocity(-velocity);
+            Motor02.setVelocity(-velocity);
+            Motor03.setVelocity(velocity);
+            Motor04.setVelocity(velocity);
+        } else {
+            Motor01.setVelocity(velocity_still);
+            Motor02.setVelocity(velocity_still);
+            Motor03.setVelocity(velocity_still);
+            Motor04.setVelocity(velocity_still);
         }
+
+
+
+
+        if (gamepad2.right_bumper) {
+            servoTurn(servo_left, -0.05f);
+            servoTurn(servo_left, -0.05f);
+        }
+
+        if (isLeftTrigger > 0) {
+            servoTurn(arm_left, 0.05f);
+        }
+
+        if (isRightTrigger > 0) {
+            servoTurn(arm_left, -0.05f);
+        }
+
+        isLeftBumperPressed = false;
+        isRightBumperPressed = false;
+    }
+
+    private void servoTurn(Servo servo, float desiredSpeed) {
+        range_motor += desiredSpeed;
+        servo.setPosition(range_motor);
+    }
+
+    private void telemetryUpdates() {
+
+        telemetry.addData("Right", isRightTrigger);
+        telemetry.addData("Left", isLeftTrigger);
+        telemetry.addData("ServoPos", servoPos);
+        telemetry.update();
     }
 }
